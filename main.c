033aa037094881054bade51dbff34dc25e9664bc
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-09 12:13:58 -0600

    Se crea un loop básico para aceptar conexiones indefinidamente

--- main.c
+++ main.c
@@ -1,53 +1,78 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <string.h>
+#include "main.h"
+#include "constants.h"
 
-#define PORT 8080
+int port = 8080;
+int server_fd;
+struct sockaddr_in address;
+int addr_len = sizeof(address);
 
-int main() {
-    int server_fd, new_socket, val_read;
-    struct sockaddr_in address;
+int init_config() {
     int opt = 1;
-    int addr_len = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server!";
 
+    printf("[Info] Creating connection socket ");
+    fflush(NULL);
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
+        perror("...failed\n");
         return -1;
     }
+    printf("...done\n");
 
+    printf("[Info] Setting socket configuration ");
+    fflush(NULL);
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("Failed to set socket options");
+        perror("...failed\n");
         return -1;
     }
+    printf("...done\n");
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        perror("Failed to bind socket");
+    printf("[Info] Binding server to address ");
+    fflush(NULL);
+    int status = bind(server_fd, (struct sockaddr *) &address, sizeof(address));
+    if (status < 0) {
+        perror("...failed\n");
         return -1;
     }
+    printf("...done\n");
 
+    printf("[Info] Listening to connections on port %i ", port);
+    fflush(NULL);
     if (listen(server_fd, 3) < 0) {
-        perror("Failed to listen");
+        perror("...failed\n");
+        return -1;
+    }
+    printf("...done\n");
+
+    return 0;
+}
+
+int main() {
+    int init_status = init_config();
+    if (init_status < 0) {
+        perror("[Error] Failed to initialize server.\n");
         return -1;
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addr_len)) < 0) {
-        perror("Error accepting connection");
-        return -1;
+    int new_socket, val_read;
+    char buffer[1024] = {0};
+    char *msg = "Hello from server!";
+
+    for (int i = 0; i < 100; i++) {
+        printf("[Info] Waiting for connection.\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &address, (socklen_t *) &addr_len)) < 0) {
+            perror("[Warning] Failed to accept incoming connection.\n");
+            continue;
+        }
+
+        val_read = read(new_socket, buffer, 1024);
+        printf("[Client] %s\n", buffer);
+        send(new_socket, msg, strlen(msg), 0);
+
+        sleep(3);
     }
-
-    val_read = read(new_socket, buffer, 1024);
-
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Message sent!\n");
 
     return 0;
 }

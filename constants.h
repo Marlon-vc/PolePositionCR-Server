3e28da7b0f76cef75c64a675258c6dced2804736
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-06-19 21:23:31 -0600

    Optimización de código

--- constants.h
+++ constants.h
@@ -5,13 +5,6 @@
 #ifndef POLEPOSITIONCR_SERVER_CONSTANTS_H
 #define POLEPOSITIONCR_SERVER_CONSTANTS_H
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-
 #define SERVER_PORT 8080
 #define TRACK_LENGTH 1800
 #define START 100
@@ -21,10 +14,5 @@
 #define LIVE 8
 #define TURBO 10
 #define BUFFER_SIZE 4096
-const cJSON *action = NULL;
-
-int server_fd, new_socket, val_read;
-struct sockaddr_in address;
-int addr_len;
 
 #endif //POLEPOSITIONCR_SERVER_CONSTANTS_H
